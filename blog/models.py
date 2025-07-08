8d0e7ed15afd2151fa5174c6af13665bc12431aa
jtate <jtate.cc@outlook.com>
2025-07-08 22:26:40 +0100

    Created initial classes and post detail view along with form for comments

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    cover_image = models.ImageField(upload_to='blog_covers/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse("blog_detail", kwargs={"slug": self.slug})
+
+
+class Comment(models.Model):
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.blog_post.title}"
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import DetailView
+from django.views.generic.edit import FormMixin
+from django.urls import reverse
+from .models import BlogPost, Comment
+from .forms import CommentForm
+
+class BlogPostDetailView(FormMixin, DetailView):
+    model = BlogPost
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+    form_class = CommentForm
+
+    def get_success_url(self):
+        return reverse("blog_detail", kwargs={"slug": self.get_object().slug})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = self.get_form()
+        context["comments"] = self.get_object().comments.filter(approved=True)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.blog_post = self.object
+            comment.save()
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
